067ec63adf2b083993b8e0881e2c6f677d9f2cc0
Giovanny L. Silva <didiosilva395@gmail.com>
2022-03-09 22:53:40 -0400

    Adicionado validação para agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,7 @@
 import Appointment from "../models/Appointment";
 import User from "../models/User";
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 class AppointmentController{
   async store(req,res){
@@ -25,10 +26,31 @@ class AppointmentController{
       return res.status(400).json({ error: 'You can only appointments with providers'});
     }
 
+    const hourStart = startOfHour(parseISO(date));
+    console.log(hourStart);
+    if(isBefore(hourStart, new Date())){
+      return res.status(400).json({ error: 'Past dates are not permitted'});
+    } 
+
+    /*
+    * Check date availability
+    */
+   const checkAvailability = await Appointment.findOne({
+     where: {
+      provider_id,
+      canceled_at: null,
+      date: hourStart
+     }
+   });
+
+   if (checkAvailability){
+    return res.status(400).json({ error: 'Appoint data is not available'});
+   }
+
     const appointment = await Appointment.create({
       user_id: req.userID,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);
