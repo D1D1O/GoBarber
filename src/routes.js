d0c72d3694e8acee61fd710cb6378f41008632a1
Giovanny L. Silva <didiosilva395@gmail.com>
2022-03-09 22:15:51 -0400

    Adicionado rota para Appointments

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -22,6 +23,8 @@ routes.put('/users' ,UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointment', AppointmentController.store);
 
 
 
