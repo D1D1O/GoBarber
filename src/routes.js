7fe548d234ad105f708b4e7e35c18b5f479e0744
Giovanny L. Silva <didiosilva395@gmail.com>
2022-03-10 22:51:07 -0400

    Listando agenda do prestador

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -26,6 +27,9 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointment', AppointmentController.store);
 routes.get('/appointment', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
+
 
 
 
