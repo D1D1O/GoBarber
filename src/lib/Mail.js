5b34d929f6bbe3d07e0200f1f762362498d73022
Giovanny L. Silva <didiosilva395@gmail.com>
2022-03-14 22:20:43 -0400

    ok

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,5 +1,12 @@
 import nodemailer from 'nodemailer';
+import { resolve }  from 'path';
 import mailConfig from '../config/mail';
+import exphbs from 'express-handlebars';
+//import nodemailerhbs from 'nodemailer-express-handlebars';
+import nodemailerhbs from 'nodemailer-handlebars';
+import { create } from 'express-handlebars';
+//const hbs = require('hbs');
+
 
 
 class Mail{
@@ -12,7 +19,32 @@ class Mail{
       secure,
       auth: auth.user ? auth : null,
     });
+
+    this.consfigureTemplates();
   }
+  consfigureTemplates(){
+    const viewPath = resolve(__dirname,'..','app','views','emails');
+    //hbs.registerPartials(__dirname + '/views/partials');
+    console.log(resolve(viewPath, 'partials'));
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: create({
+          extname: '.hbs',
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
+
+
+  }
+
   sendMail(message){
     return this.transporter.sendMail({
       ...mailConfig.default,
